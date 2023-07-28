b5b49dd5236184f15f5544abea9ede595f34d1df
mili <49826851+milnorms@users.noreply.github.com>
2023-07-27 19:55:31 -1000

    fixed print

--- 06_breadth-first_search/directedGraph.py
+++ 06_breadth-first_search/directedGraph.py
@@ -56,54 +56,58 @@ class Node:
 # e.addChildNode(f)
 
 # Initialize nodes and their values
-a = Node(0, isStart=True)
-b = Node(1)
-c = Node(2)
-d = Node(3)
+a = Node('a', isStart=True)
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
 
 
 # Create relationships
 a.addChildNode(b)
+a.addChildNode(c)
 
 b.addChildNode(c)
 
-c.addChildNode(a)
 c.addChildNode(b)
+c.addChildNode(d)
 
-d.addChildNode(c)
+d.addChildNode(e)
 
 
 class DirectedGraph:
-    def __init__(self, nodes:[Node]):
+    def __init__(self, nodes: [Node]):
         self.nodes = nodes
     
-    def isParent(self, node1, node2):
+    def isParent(self, node1: Node, node2: Node):
         return node1 in node2.parents
     
-    def isChild(self, node1, node2):
+    def isChild(self, node1: Node, node2: Node):
         return node1 in node2.children
         
-    def areConnected(self, node1, node2):
+    def areConnected(self, node1: Node, node2: Node):
         return self.isParent(node1, node2) or self.isParent(node2, node1) or self.isChild(node1, node2) or self.isChild(node2, node1)
     
     def __str__(self):
         # Print adjacency list
         output = ''
-        for n in self.nodes:
-            for child in n.children:
-                output+=f'{n} -> {child} \n'
+        for node in self.nodes:
+            output+=f'\n {node} ->'
+            for child in node.children:
+                output+=f' {child}'
         
         return output   
         
 # graph = DirectedGraph([a, b, c, d, e, f])
-graph = DirectedGraph([a, b, c, d])
+graph = DirectedGraph([a, b, c, d, e])
 print(graph)
+print(b.parents)
 
 '''
 todo: 
-- fix str output. change to match this: https://www.techiedelight.com/graph-implementation-python/
-- accept any input as node but stringify in node class
-- implement DFS and DFS
+[X] fix str output. change to match this: https://www.techiedelight.com/graph-implementation-python/
+[X] accept any input as node but stringify in node class
+[] implement DFS and DFS
 '''
 
 
